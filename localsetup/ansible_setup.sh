093ce4b278f1c1137115c6b7791d01580c831f1c
wate <pc.poisoning@gmail.com>
2018-05-09 02:14:31 +0900

    fix

--- localsetup/ansible_setup.sh
+++ localsetup/ansible_setup.sh
@@ -5,9 +5,9 @@
 
 echo "1. Check sources.list"
 
-source_path="/etc/apt/sources.list" 
+source_path="/etc/apt/sources.list"
 deb_setting="deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main"
-grep_result=`grep "${deb_setting}" "${source_path}"`
+grep_result=$(grep "${deb_setting}" "${source_path}")
 if [ -n "${grep_result}" ]; then
     echo "> already inserted source."
 else
@@ -16,13 +16,14 @@ else
 fi
 
 echo "2. add key"
+apt-get install -y dirmngr
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 
 echo "3. apt-get update"
 apt-get update
 
 echo "4. install ansible"
-apt-get install ansible
+apt-get install -y ansible
 
 echo "5. ansible version"
 ansible --version
