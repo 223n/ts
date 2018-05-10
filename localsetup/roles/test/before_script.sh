12620ae98c0c148a09215423fd22d0a018164f68
223n <223n@223n.tech>
2018-05-10 22:54:30 +0900

    fix do roop and copy script.

--- localsetup/roles/test/before_script.sh
+++ localsetup/roles/test/before_script.sh
@@ -1,15 +1,33 @@
 #!/bin/sh
 
+COPY_FROM="./localsetup/roles"
 WORK_DIR="./localsetup/roles/test"
+roles=(
+  "etckeeper"
+  "git"
+  "gnucash"
+  "stride"
+  "vscode"
+)
+dirs=(
+  "defaults"
+  "handler"
+  "meta"
+  "tasks"
+  "templates"
+)
 
-for role in "etckeeper git gnucash stride vscode"; do
-    ROLE_DIR="${WORK_DIR}/roles/${role}"
-    if [ -d ${ROLE_DIR} ]; then
-        rm -rf "${ROLE_DIR}/*"
-    else
-        mkdir -p "${ROLE_DIR}"
+for role in "${roles[@]}"; do
+  ROLE_FROM="${COPY_FROM}/${role}"
+  ROLE_DIR="${WORK_DIR}/roles/${role}"
+  if [ -d "${ROLE_DIR}" ]; then
+    rm -rf "${ROLE_DIR}/*"
+  else
+    mkdir -p "${ROLE_DIR}"
+  fi
+  for dir in "${dirs[@]}"; do
+    if [ -d "${ROLE_FROM}/${dir}" ]; then
+      cp -pR "${ROLE_FROM}/${dir}" "${ROLE_DIR}";
     fi
-    for dir in "defaults handler meta tasks templates"; do
-        cp -pR "${dir}" "${ROLE_DIR}";
-    done
+  done
 done
