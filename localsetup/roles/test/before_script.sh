d6f725e709541d8e1012b871d054e02adb11dda9
223n <223n@223n.tech>
2018-05-10 22:37:00 +0900

    fix script typo

--- localsetup/roles/test/before_script.sh
+++ localsetup/roles/test/before_script.sh
@@ -2,14 +2,14 @@
 
 WORK_DIR="./localsetup/roles/test"
 
-for role in "etckeeper git gnucash stride vscode";
+for role in "etckeeper git gnucash stride vscode"; do
     ROLE_DIR="${WORK_DIR}/roles/${role}"
     if [ -d ${ROLE_DIR} ]; then
         rm -rf "${ROLE_DIR}/*"
     else
         mkdir -p "${ROLE_DIR}"
     fi
-    for dir in "defaults handler meta tasks templates";
-        do cp -pR "${dir}" "${ROLE_DIR}";
+    for dir in "defaults handler meta tasks templates"; do
+        cp -pR "${dir}" "${ROLE_DIR}";
     done
 done
